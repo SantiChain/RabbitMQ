246b91a2da65cd451e45f30ae35a72fdda7948de
SantiChain <117780948+SantiChain@users.noreply.github.com>
2024-03-22 19:41:07 -0500

    Create producer.js

---
+++ rabbitmq-nodejs/producer.js
@@ -0,0 +1,19 @@
+const amqp = require('amqplib');
+
+async function main() {
+    const connection = await amqp.connect('amqp://localhost');
+    const channel = await connection.createChannel();
+
+    const queueName = 'HOLA DESDE JAVASCRIPT';
+    const message = 'MENSAJE DE JAVASCRIPT!';
+
+    await channel.assertQueue(queueName, { durable: true });
+    await channel.sendToQueue(queueName, Buffer.from(message), { persistent: true });
+
+    console.log([x] Sent '${message}');
+
+    await channel.close();
+    await connection.close();
+}
+
+main().catch(console.error);
