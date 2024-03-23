96489cf5d372336278ab31d1bb0d63ee6ed9a11f
SantiChain <117780948+SantiChain@users.noreply.github.com>
2024-03-22 19:41:28 -0500

    Update consumer.js

--- rabbitmq-nodejs/consumer.js
+++ rabbitmq-nodejs/consumer.js
@@ -1,1 +1,17 @@
+const amqp = require('amqplib');
 
+async function main() {
+    const connection = await amqp.connect('amqp://localhost');
+    const channel = await connection.createChannel();
+
+    const queueName = 'hello';
+
+    await channel.assertQueue(queueName, { durable: true }); 
+    console.log('[*] Waiting for messages. To exit, press CTRL+C');
+
+    channel.consume(queueName, (msg) => {
+        console.log([x] Received ${msg.content.toString()});
+    }, { noAck: true });
+}
+
+main().catch(console.error);
