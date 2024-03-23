aa0d72d7e8b4dbbde79bee572b7b2a29ca3241bc
SantiChain <117780948+SantiChain@users.noreply.github.com>
2024-03-22 19:48:02 -0500

    Update producer.py

--- rabbitmq-python/producer.py
+++ rabbitmq-python/producer.py
@@ -3,8 +3,8 @@ import pika
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 
-queue_name = 'MENSAJE DE PYTHON'
-message = 'HOLA JAVASCRIPT DESDE PYTHON!'
+queue_name = 'hola'
+message = 'MENSAJE DE PYTHON'
 
 channel.queue_declare(queue=queue_name, durable=True)  # Establece durable en True
 channel.basic_publish(exchange='', routing_key=queue_name, body=message, properties=pika.BasicProperties(delivery_mode=2))  # Establece delivery_mode en 2
